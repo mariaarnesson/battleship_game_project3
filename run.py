f89cf799696687b908f54abcf9447c5d0d617c0e
mariaarnesson <maria_arnesson@outlook.com>
2022-12-09 10:01:56 +0000

    Add a function named create_ships

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ def main_menu():
 
 main_menu()
 
+#create a game board
 
 board = [
     ['_', '_', '_', '_', '_', '_', '_'],
@@ -15,8 +16,30 @@ board = [
     ['_', '_', '_', '_', '_', '_', '_'],
 ]
 
+letters_to_numbers = {
+    'A' = 0,
+    'B' = 1,
+    'C' = 2,
+    'D' = 3,
+    'E' = 4,
+    'F' = 5,
+    'G' = 6
+}
+
 
 
 def create_ships():
+
+    column = input('Please, choose some letter between A-G: ')
+    while column not in 'ABCDEFG':
+        print('Incorrect! You should choose A, B, C, D, E, F or G')
+        column = input('Please, choose a letter between A-G')
+
+    row = input('Please, choose the row 1-7 of the ship')  
+    while row not in '1234567':
+        print('Incorrect! You should choose 1, 2, 3, 4, 5, 6 or 7')
+        row = input('Please, choose a letter between 1-7')  
+
+    return int(row) -1, letters_to_numbers[column]    
 
 def print_board(board):    
